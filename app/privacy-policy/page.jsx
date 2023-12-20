a882ba1edc8fb74bad6f0fb3bcf1de20ceacfaf2
PromiseUdo <info.promiseudo@gmail.com>
2023-12-20 08:12:54 +0100

    privacy-policy

--- app/privacy-policy/page.jsx
+++ app/privacy-policy/page.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import BreadCrumb from "../components/BreadCrumb";
+import PageContent from "./components/PageContent";
 
 const page = () => {
   return (
     <div>
       <BreadCrumb />
+      <PageContent />
     </div>
   );
 };
